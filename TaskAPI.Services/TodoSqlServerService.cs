3dd78eecb38156bf8805f7439be6f0ce077fe914
Fiqri Ismail <fiqri.ismail@gmail.com>
2021-05-22 20:43:59 +0530

    Step 5 Completed

---
+++ TaskAPI.Services/TodoSqlServerService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskAPI.Models;
+using TaskAPI.DataAccess;
+
+namespace TaskAPI.Services
+{
+    public class TodoSqlServerService : ITodoRepository
+    {
+        private readonly TodoDbContext _context = new TodoDbContext();
+        public List<Todo> AllTodos()
+        {
+            return _context.Todos.ToList();
+        }
+    }
+}
